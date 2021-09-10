56c9635975fac6c9a5353de5d4d7a98ef4839150
shev-81 <shev-81@mail.ru>
2021-09-10 15:43:37 +0300

    Домашнее задание к 7 лекции
    Доработан код. и Комментарии в поставленных задачах.

--- lesson_7/HomeWork7.java
+++ lesson_7/HomeWork7.java
@@ -5,15 +5,24 @@ package lesson_7;
  */
 public class HomeWork7 {
     public static void main(String[] args) {
-        //свора голодных котов
+/** п.4  Создать массив котов и тарелку с едой, попросить всех котов покушать
+*        из этой тарелки и потом вывести информацию о сытости котов в консоль.
+*/
+        // дадим пустую тарелку
+        Plate plate = new Plate();
+
+        // наполним ее едой
+        plate.setFood(100);
+
+        //позовем свору голодных котиков
         Cat [] catArray = new Cat [5];
         catArray[0] = new Cat("Barsik", 5);
         catArray[1] = new Cat("Мурзик", 10);
         catArray[2] = new Cat("Кот Артем", 90);
         catArray[3] = new Cat("Арбузик", 5);
         catArray[4] = new Cat("Тишка", 20);
-        Plate plate = new Plate(100);
-        // покормим свору голодных кисек
+
+        // покормим голодных кисек
         for(Cat cats: catArray){
             cats.eat(plate);
             plate.info();
--- lesson_7/Plate.java
+++ lesson_7/Plate.java
@@ -1,16 +1,28 @@
 package lesson_7;
 
 /**
- * Класс корма из тарелки для котов
+ * Класс тарелки для котов
  */
 
 public class Plate {
     private int food;
+
+    public Plate() {
+        this.food = 0;
+    }
     public Plate(int food) {
         this.food = food;
     }
     public void decreaseFood(int n) {
         food -= n;
+    }
+
+/**
+*   п.5 Добавить в тарелку метод, с помощью которого можно было бы добавлять еду в тарелку.
+*/
+    public void setFood(int food) {
+        if(food>0)this.food = food;
+            else System.out.printf("Тарелка не должна быть пустой");
     }
 
     public int getFood() {
