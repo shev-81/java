68025be0420d31234760028e5149406f95cbd72a
shev-81 <shev-81@mail.ru>
2021-10-07 22:18:57 +0300

    Домашнее задание №2 по Алгоритмам

--- algoritms_lesson_1/MainClass.java
+++ algoritms_lesson_1/MainClass.java
@@ -1,6 +1,8 @@
 package algoritms_lesson_1;
-import java.util.Random;
 
+/**
+ *  Домашнее задание Шевеленко Андрея ко 2-ой лекции .
+ */
 public class MainClass {
     /**
      *  Создание массива обьектов ноутбуков
@@ -12,12 +14,10 @@ public class MainClass {
             massive[i] = new MyNbook();
         }
     return massive;
-
     }
     public static void startTime() {
         time = System.currentTimeMillis();
     }
-
     public static void endTime() {
         time = System.currentTimeMillis() - time;
         System.out.println(time);
@@ -42,7 +42,6 @@ public class MainClass {
                     min = j;
                 }
             }
-
             if (i == min) {
                 continue;
             }
@@ -54,13 +53,14 @@ public class MainClass {
 
     public static void main(String[] args) {
         MyNbook [] arr;
-        arr =getArray();
+        arr = getArray();
         System.out.println("Это не отсортированный массив");
         for (MyNbook u: arr) {
             System.out.println("Производитель : "+u.nameBook+", Память : "+ u.memory+", Цена : "+u.cost);
         }
         startTime();
         Selectsort(arr);
+        endTime();
         System.out.println("А это Отсортированный");
         for (MyNbook u: arr) {
             System.out.println("Производитель : "+u.nameBook+", Память : "+ u.memory+", Цена : "+u.cost);
@@ -69,38 +69,8 @@ public class MainClass {
         endTime();
     }
 }
-/** Класс создания экземпляра ноутбука
- *  Lenuvo > Asos > MacNote > Eser > Xamiou
- */
 
-class MyNbook{
-    private static final Random random = new Random();
-    int cost;
-    int memory;
-    int numBook;
-    String nameBook;
 
-    MyNbook(){
-        cost = random.nextInt(40)*50;
-        memory = random.nextInt(6)*4;
-        numBook = random.nextInt(5);
-        if (memory==0) memory=4;
-        if (cost<500) cost=cost+500;
-        switch (numBook) {
-            case 0: nameBook="Xamiou";
-                break;
-            case 1: nameBook="Eser";
-                break;
-            case 2: nameBook="MacNote";
-                break;
-            case 3: nameBook="Asos";
-                break;
-            case 4: nameBook="Lenuvo";
-                break;
-            default: nameBook = "Xamiou";
-        }
-    }
-}
 
 
 
