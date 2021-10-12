c636ee7dab8a5f53c978032a0674eb5d1060b6c1
shev-81 <shev-81@mail.ru>
2021-10-12 13:38:58 +0300

    ДЗ к  3 Лекции Алгоритмы  для сдачи преподавателю

--- algoritm_lesson_3/Queue.java
+++ algoritm_lesson_3/Queue.java
@@ -1,5 +1,13 @@
 package algoritm_lesson_3;
 
+/**
+ *
+ *   Домашняя работа к 3 лекции Шевеленко Андрея
+ *
+ *   От Queue<E> интерфейса реализованно 2 класса - QueueMassive<E> и
+ *   DeQueue<E> расширяющий класс QueueMassive<E>.
+ */
+
 public interface Queue<E> {
 
     boolean insert(E value);
