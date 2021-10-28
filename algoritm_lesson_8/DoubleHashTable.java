0a2ee43fa192e7e9b7c4751e3096c4d7a3d5b739
shev-81 <shev-81@mail.ru>
2021-10-28 10:42:15 +0300

    Начало работы над ДЗ 8 по алгоритмам

---
+++ algoritm_lesson_8/DoubleHashTable.java
@@ -0,0 +1,16 @@
+package algoritm_lesson_8;
+
+public class DoubleHashTable<K, V> extends HashTableImpl<K, V> {
+    public DoubleHashTable(int initialCapacity) {
+        super(initialCapacity);
+    }
+
+    @Override
+    protected int getStep(K key) {
+        return hashDoubleFunc(key);
+    }
+
+    private int hashDoubleFunc(K key) {
+        return 5 - (key.hashCode() % 5);
+    }
+}
