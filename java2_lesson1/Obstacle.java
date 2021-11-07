ea6c0e188c6f81ae2b48b1871ea4ea5a6b0ed231
shev-81 <shev-81@mail.ru>
2021-11-07 19:19:56 +0300

    Удалена ДЗ1

--- java2_lesson1/Obstacle.java
+++
@@ -1,6 +0,0 @@
-package java2_lesson1;
-// Препятствие
-public interface Obstacle {
-    boolean canDoit(Participant can);
-    String toString();
-}
--- java2_lesson1/Participant.java
+++
@@ -1,7 +0,0 @@
-package java2_lesson1;
-// участники соревнований
-public interface Participant {
-    String getName();
-    int getMaxrun();
-    int getMaxjump();
-}
