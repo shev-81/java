8362fa19e8c3071c953edd6356d14414e031a3fa
shev-81 <shev-81@mail.ru>
2021-11-09 21:48:30 +0300

    подготовка к сдаче 2 дз по яве
    Предварительная очистка ветки

--- java2_lesson2/MyArraySizeException.java
+++
@@ -1,8 +0,0 @@
-package java2_lesson2;
-
-public class MyArraySizeException extends RuntimeException{
-    @Override
-    public void printStackTrace(){
-        System.out.println("Задан не верный размер массива");
-    }
-}
