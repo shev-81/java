c4545e814f4cc0614ef9f2cafe742412c1854a94
shev-81 <shev-81@mail.ru>
2021-11-19 22:47:50 +0300

    Добавлен цикл в первый метод
    Добавлен цикл 
    for (int i=0;i<SIZE;i++){arr[i] = 1.0f;} 
    
    в первый метод для соблюдения условия ДЗ.

--- java2_lesson5/HomeWorkDz.java
+++ java2_lesson5/HomeWorkDz.java
@@ -9,6 +9,7 @@ public class HomeWorkDz {
     // однопоточный метод обработки
     public static void firstMetod() {
         float[] arr = new float[SIZE];
+        for (int i=0;i<SIZE;i++){arr[i] = 1.0f;}
         long startTime = System.currentTimeMillis();
         for (int i=0;i<SIZE;i++){
             arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
